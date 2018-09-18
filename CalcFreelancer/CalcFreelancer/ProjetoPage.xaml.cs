a7694c4911c65526723758a9449c2e4a552042fd
Marcio Vinicius <marcio.campos@outlook.com>
2018-09-17 23:03:15 -0300

    add ViewModel para Projeto

--- CalcFreelancer/CalcFreelancer/ProjetoPage.xaml.cs
+++ CalcFreelancer/CalcFreelancer/ProjetoPage.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using CalcFreelancer.Repository;
+using CalcFreelancer.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -16,39 +17,9 @@ namespace CalcFreelancer
 		public ProjetoPage ()
 		{
 			InitializeComponent ();
-            GravarButton.Clicked += GravarButton_Clicked;
-            LimparButton.Clicked += LimparButton_Clicked;
+            var viewModel = new ProjetoPageViewModel();
+            BindingContext = viewModel;
         }
 
-        private void GravarButton_Clicked(object sender, EventArgs e)
-        {
-            var valorTotal = double.Parse(ValorPorHora.Text) * int.Parse(HorasPorDia.Text) * int.Parse(DiasDuracaoProjeto.Text);
-            ValorTotal.Text = $"{valorTotal.ToString("C")}";
-            Gravar(valorTotal);
-        }
-        private async void Gravar(double valorTotal)
-        {
-            var projetoAzureClient = new AzureProjetoRepository();
-
-            projetoAzureClient.Insert(new Models.Projeto()
-            {
-                Nome = Nome.Text,
-                ValorPorHora = double.Parse(ValorPorHora.Text),
-                HorasPorDia = int.Parse(HorasPorDia.Text),
-                DiasDuracaoProjeto = int.Parse(DiasDuracaoProjeto.Text),
-                ValorTotal = valorTotal
-            });
-
-            await App.Current.MainPage.DisplayAlert("Sucesso", "Projeto gravado!", "Ok");
-        }
-
-        private void LimparButton_Clicked(object sender, EventArgs e)
-        {
-            Nome.Text = string.Empty;
-            ValorPorHora.Text = string.Empty;
-            HorasPorDia.Text = string.Empty;
-            DiasDuracaoProjeto.Text = string.Empty;
-            ValorTotal.Text = string.Empty;
-        }
     }
 }
---
+++ CalcFreelancer/CalcFreelancer/ViewModels/ProjetoPageViewModel.cs
@@ -0,0 +1,106 @@
+﻿using CalcFreelancer.Repository;
+using CalcFreelancer.ViewModels.Base;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace CalcFreelancer.ViewModels
+{
+    class ProjetoPageViewModel : ViewModelBase
+    {        
+        private string nome;
+        public string Nome
+        {
+            get { return nome; }
+            set { SetProperty(ref nome, value); }
+        }
+
+        private double valorPorHora;
+        public double ValorPorHora
+        {
+            get { return valorPorHora; }
+            set
+            {
+                SetProperty(ref valorPorHora, value);
+                CalcularValorTotal();
+            }
+        }
+
+        private int horasPorDia;
+        public int HorasPorDia
+        {
+            get { return horasPorDia; }
+            set
+            {
+                SetProperty(ref horasPorDia, value);
+                CalcularValorTotal();
+            }
+        }
+
+        private int diasDuracaoProjeto;
+        public int DiasDuracaoProjeto
+        {
+            get { return diasDuracaoProjeto; }
+            set
+            {
+                SetProperty(ref diasDuracaoProjeto, value);
+                CalcularValorTotal();
+            }
+        }
+
+        private double valorTotal;
+        public double ValorTotal
+        {
+            get { return valorTotal; }
+            set { SetProperty(ref valorTotal, value); }
+        }
+
+        private void CalcularValorTotal()
+        {
+            if (ValorPorHora > 0 && HorasPorDia > 0 && DiasDuracaoProjeto > 0)
+            {
+                ValorTotal = ValorPorHora * HorasPorDia * DiasDuracaoProjeto;
+            }
+            else
+            {
+                ValorTotal = 0;
+            }
+        }
+
+        public Command GravarCommand { get; }
+        public Command LimparCommand { get; }
+
+
+        public ProjetoPageViewModel()
+        {
+            GravarCommand = new Command(ExecuteGravarCommand);
+            LimparCommand = new Command(ExecuteLimparCommand);
+        }
+
+        private void ExecuteLimparCommand()
+        {
+            Nome = string.Empty;
+            HorasPorDia = 0;
+            DiasDuracaoProjeto = 0;
+        }
+
+        private async void ExecuteGravarCommand()
+        {
+            var projetoAzureClient = new AzureProjetoRepository();
+
+            projetoAzureClient.Insert(new Models.Projeto()
+            {
+                ValorPorHora = ValorPorHora,
+                HorasPorDia = HorasPorDia,
+                DiasDuracaoProjeto = DiasDuracaoProjeto,
+                ValorTotal = ValorTotal
+            });
+
+
+            ExecuteLimparCommand();
+
+            await App.Current.MainPage.DisplayAlert("Sucesso", "Projeto gravado!", "Ok");
+        }
+    }
+}
